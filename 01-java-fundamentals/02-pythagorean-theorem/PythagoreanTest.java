2582c35ef933d3e54de2d96ab953064eb07fb59d
Christopher Castaneda <christophercastaneda@ChristophersMBP.home>
2020-12-21 22:38:22 -0600

    added Pythagorean file

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean p = new Pythagorean();
+        Double hypotenuse = p.calculateHypotenuse(6,8);
+        System.out.println(hypotenuse);
+    }
+}
